1a8fa6e701c79bc03af755fd6932ba1c0addfeb7
Rashmi Patil <hiitsrash24@gmail.com>
2024-05-09 23:10:39 +0530

    Resolved responsive issue

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import about from "../../assets/images/about.jpg";
 
+// About component displaying information about the platform
 const About = () => {
   return (
     <>
       <div className="container">
-        <div className="flex items-center justify-between gap-[50px] lg:gap[130px] xl:gap-flex-col lg:flex-row">
-          <div className="relative w-3/4 lg:w-1/2 xl:w-[700px] z-10 order-2 lg:order-1">
+        {/* Flex container for responsive layout */}
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-8 lg:gap-16">
+          {/* Image section */}
+          <div className="relative  w-full lg:w-1/2 xl:w-1/3 order-1 lg:order-2">
             <img
               src={about}
               alt="about-img"
               className="h-[800px] w-fit object-cover"
             />
-            {/* <div className="absolute z-20 bottom-4 w-[200px] md:w-[200px] right-[-30%] md:right-[-7%] lg:right-[22%]">
-              <img src="" alt="aboutCard-img" />
-            </div> */}
           </div>
 
-          <div className="w-full lg:w-1/2 xl:w-[670px] order-1 lg:order-2">
+          <div className="w-full lg:w-1/2 xl:w-[670px] order-2 lg:order-1">
             <h2 className="heading">
               Join thousands of satisfied users & experience the future of
               healthcare booking with Cure today!
             </h2>
-            <p className="text-para">
+            <p className="text-para mt-4">
               Cure is a revolutionary online platform dedicated to simplifying
               the process of booking appointments with healthcare professionals.
               With the mission of providing accessible and convenient healthcare
@@ -31,7 +31,7 @@ const About = () => {
               connects patients with qualified doctors and medical facilities.
             </p>
 
-            <p className="text-para mt-[30px]">
+            <p className="text-para mt-8">
               Our platform was born out of the recognition that navigating the
               healthcare system can be overwhelming and time-consuming. Whether
               you're seeking routine check-ups, specialized treatments, or
@@ -43,8 +43,9 @@ const About = () => {
               physicians to specialists in fields such as cardiology,
               dermatology, and pediatrics, we've got you covered.
             </p>
+            {/* Learn More button linking to homepage */}
             <Link to="/">
-              <button className="btn">Learn More</button>
+              <button className="btn mt-8">Learn More</button>
             </Link>
           </div>
         </div>
