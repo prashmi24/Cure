c250207399511ec5f635abe47448312a95b56fcf
Rashmi Patil <hiitsrash24@gmail.com>
2024-05-12 15:05:36 +0530

    Modified the condition
    Modified the condition to check if user data exists in local storage to prevent errors.

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,15 +1,18 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 
+// Defined initial state for authentication context
 const initialState = {
-  user:
-    localStorage.getItem("user") !== undefined
-      ? JSON.parse(localStorage.getItem("user"))
-      : null,
+  user: localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null,
   role: localStorage.getItem("role") || null,
   token: localStorage.getItem("token") || null,
 };
 
+// Created authentication context
 export const AuthContext = createContext(initialState);
+
+// Defined authentication reducer function
 const authReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN_START":
@@ -35,13 +38,17 @@ const authReducer = (state, action) => {
   }
 };
 
+// AuthContextProvider component to provide authentication context to children
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
+
+  // Update local storage whenever authentication state changes
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(state.user));
     localStorage.setItem("token", state.token);
     localStorage.setItem("role", state.role);
   }, [state]);
+
   return (
     <AuthContext.Provider
       value={{
