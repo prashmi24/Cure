c0fd75cc71ff0cedf6ed838ead615399e1fd1c68
Rashmi Patil <hiitsrash24@gmail.com>
2024-09-14 16:15:48 +0530

    Added Fallback Message

--- client/src/dashboard/doctor/Appointments.jsx
+++ client/src/dashboard/doctor/Appointments.jsx
@@ -3,76 +3,89 @@ import { dateFormat } from "../../utils/dateFormat";
 
 const Appointments = ({ appointments }) => {
   return (
-    <table className="w-full text-left text-sm text-gray-500">
-      {/* Table Header */}
-      <thead className="text-xs text-white uppercase bg-yellowColor">
-        <tr>
-          <th scope="col" className="px-6 py-3">
-            Name
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Gender
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Payment Status
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Payment Amount
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Booked on
-          </th>
-        </tr>
-      </thead>
+    <div>
+      {appointments && appointments.length > 0 ? (
+        <table className="w-full text-left text-sm text-gray-500">
+          {/* Table Header */}
+          <thead className="text-xs text-white uppercase bg-yellowColor">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Gender
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Payment Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Payment Amount
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Booked on
+              </th>
+            </tr>
+          </thead>
 
-      {/* Table Body */}
-      <tbody>
-        {appointments?.map((item) => (
-          <tr key={item._id}>
-            {/* User Info */}
-            <th
-              scope="row"
-              className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
-            >
-              <img
-                src={item.user.photo}
-                alt="user-img"
-                className="w-10 h-10 rounded-full"
-              />
-              <div className="pl-3">
-                <div className="text-base font-semibold">{item.user.name}</div>
-                <div className="text-normal text-gray-500">
-                  {item.user.email}
-                </div>
-              </div>
-            </th>
-            {/* Gender */}
-            <td className="px-6 py-4 ">{item.user.gender}</td>
-            {/* Payment Status */}
-            <td className="px-6 py-4 ">
-              {item.isPaid && (
-                <div className="flex items-center">
-                  <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
-                  Paid
-                </div>
-              )}
-              {!item.isPaid && (
-                <div className="flex items-center">
-                  <div className="h-2.5 w-2.5 rounded-full bg-red mr-2"></div>
-                  Unpaid
-                </div>
-              )}
-            </td>
+          {/* Table Body */}
+          <tbody>
+            {appointments.map((item) => (
+              <tr key={item._id} className="hover:bg-gray-100">
+                {/* User Info */}
+                <th
+                  scope="row"
+                  className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap"
+                >
+                  <img
+                    src={item.user.photo}
+                    alt={`${item.user.name}-img`}
+                    className="w-10 h-10 rounded-full"
+                  />
+                  <div className="pl-3">
+                    <div className="text-base font-semibold">
+                      {item.user.name}
+                    </div>
+                    <div className="text-normal text-gray-500">
+                      {item.user.email}
+                    </div>
+                  </div>
+                </th>
+                {/* Gender */}
+                <td className="px-6 py-4 ">{item.user.gender}</td>
+                {/* Payment Status */}
+                <td className="px-6 py-4 ">
+                  {item.isPaid ? (
+                    <div className="flex items-center">
+                      <div
+                        className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"
+                        aria-label="Paid"
+                      ></div>
+                      Paid
+                    </div>
+                  ) : (
+                    <div className="flex items-center">
+                      <div
+                        className="h-2.5 w-2.5 rounded-full bg-red mr-2"
+                        aria-label="Unpaid"
+                      ></div>
+                      Unpaid
+                    </div>
+                  )}
+                </td>
 
-            {/* Fees */}
-            <td className="px-6 py-4 ">{item.amount}</td>
+                {/* Payment Amount */}
+                <td className="px-6 py-4 ">{item.amount}</td>
 
-            {/* Booked On */}
-            <td className="px-6 py-4 ">{dateFormat(item.createdAt)}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+                {/* Booked On */}
+                <td className="px-6 py-4 ">{dateFormat(item.createdAt)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center text-gray-500">No appointments available.</p>
+      )}
+    </div>
   );
 };
 
