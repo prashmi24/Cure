757cfca090f32d5458e4529d6cdc781a1c0d5a44
Rashmi Patil <hiitsrash24@gmail.com>
2024-09-16 20:10:36 +0530

    Formatted price

--- client/src/pages/Doctors/SidePanel.jsx
+++ client/src/pages/Doctors/SidePanel.jsx
@@ -5,7 +5,7 @@ const SidePanel = ({ doctorId, amount, timeSlots }) => {
   return (
     <div className="shadow-panelShadow p-3 lg:p-5 rounded-md w-[300px]">
       <div className="flex items-center justify-between">
-        <p className="text-para mt-0 font-semibold">${amount}</p>
+        <p className="text-para mt-0 font-semibold">${amount.toFixed(2)}</p>
         <span className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold"></span>
       </div>
 
@@ -13,21 +13,35 @@ const SidePanel = ({ doctorId, amount, timeSlots }) => {
         <p className="text-para mt-0 font-semibold text-headingColor">
           Available Time Slots:
         </p>
-        <ul className="mt-3">
-          {timeSlots?.map((item, index) => (
-            <li key={index} className="flex items-center justify-between mb-2">
-              <p className="text-[15px] leading-6 text-textColor font-semibold">
-                {item.day.charAt(0).toUpperCase() + item.day.slice(1)}
-              </p>
-              <p className="text-[15px] leading-6 text-textColor font-semibold">
-                {convertTime(item.startingTime)}- {convertTime(item.endingTime)}
-              </p>
-            </li>
-          ))}
-        </ul>
+
+        {timeSlots?.length > 0 ? (
+          <ul className="mt-3">
+            {timeSlots.map((item, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-between mb-2"
+              >
+                <p className="text-[15px] leading-6 text-textColor font-semibold">
+                  {item.day.charAt(0).toUpperCase() + item.day.slice(1)}
+                </p>
+                <p className="text-[15px] leading-6 text-textColor font-semibold">
+                  {convertTime(item.startingTime)}-{" "}
+                  {convertTime(item.endingTime)}
+                </p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-[14px] text-red-500 mt-3">No available slots</p>
+        )}
       </div>
 
-      <button className="btn px-2 w-full rounded-md">Book Appointment</button>
+      <button
+        className="btn px-2 w-full rounded-md"
+        disabled={!timeSlots?.length}
+      >
+        Book Appointment
+      </button>
     </div>
   );
 };
