1a4f04033de43d71e0cf9cbc403fb81e020edfcd
Rashmi Patil <hiitsrash24@gmail.com>
2024-09-16 16:48:27 +0530

    Handled edge cases

--- client/src/pages/Doctors/DoctorAbout.jsx
+++ client/src/pages/Doctors/DoctorAbout.jsx
@@ -14,54 +14,59 @@ const DoctorAbout = ({ name, about, qualifications, experiences }) => {
         <p className="text-para">{about}</p>
       </div>
 
-      <div className="mt-12">
-        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
-          Education
-        </h3>
-        <ul className="pt-4 md:p-5">
-          {qualifications?.map((item, index) => (
-            <li
-              key={index}
-              className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]"
-            >
-              <div>
-                <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                  {dateFormat(item.startingDate)} -{" "}
-                  {dateFormat(item.endingDate)}
+      {qualifications?.length > 0 && (
+        <div className="mt-12">
+          <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+            Education
+          </h3>
+          <ul className="pt-4 md:p-5">
+            {qualifications.map((item, index) => (
+              <li
+                key={index}
+                className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]"
+              >
+                <div>
+                  <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                    {dateFormat(item.startingDate) || "N/A"} -{" "}
+                    {dateFormat(item.endingDate) || "Present"}
+                  </span>
+                  <p className="text-[16px] leading-6 font-medium text-textColor">
+                    {item.degree}
+                  </p>
+                </div>
+                <p className="text-[14px] leading-5 font-medium text-textColor">
+                  {item.university}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {experiences?.length > 0 && (
+        <div className="mt-12">
+          <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+            Experience
+          </h3>
+
+          <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+            {experiences.map((item, index) => (
+              <li key={index} className="p-4 rounded bg-[#fff9ea]">
+                <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+                  {dateFormat(item.startingDate) || "N/A"} - {""}{" "}
+                  {dateFormat(item.endingDate) || "Present"}
                 </span>
                 <p className="text-[16px] leading-6 font-medium text-textColor">
-                  {item.degree}
+                  {item.position}
                 </p>
-              </div>
-              <p className="text-[14px] leading-5 font-medium text-textColor">
-                {item.university}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="mt-12">
-        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
-          Experience
-        </h3>
-
-        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
-          {experiences?.map((item, index) => (
-            <li key={index} className="p-4 rounded bg-[#fff9ea]">
-              <span className="text-yellowColor text-[15px] leading-6 font-semibold">
-                {dateFormat(item.startingDate)} - {dateFormat(item.endingDate)}
-              </span>
-              <p className="text-[16px] leading-6 font-medium text-textColor">
-                {item.position}
-              </p>
-              <p className="text-[14px] leading-5 font-medium text-textColor">
-                {item.hospital}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </div>
+                <p className="text-[14px] leading-5 font-medium text-textColor">
+                  {item.hospital}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
