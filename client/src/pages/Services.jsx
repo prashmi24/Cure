b46c7cfa7d7dd8df37a0471840fecb583b555244
Rashmi Patil <hiitsrash24@gmail.com>
2024-09-16 20:56:01 +0530

    removed index prop

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -4,11 +4,11 @@ import ServiceCard from "../components/Services/ServiceCard";
 
 const Services = () => {
   return (
-    <section>
+    <section className="py-10">
       <div className="container">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {services.map((item, index) => (
-            <ServiceCard item={item} index={index} key={index} />
+          {services.map((item) => (
+            <ServiceCard key={item.id} item={item} />
           ))}
         </div>
       </div>
