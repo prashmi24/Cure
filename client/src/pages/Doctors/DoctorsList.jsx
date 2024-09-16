ae82fe8be04d3177610cd02b2773dfde9a510fc4
Rashmi Patil <hiitsrash24@gmail.com>
2024-09-16 17:14:59 +0530

    Handled empty state

--- client/src/pages/Doctors/DoctorsList.jsx
+++ client/src/pages/Doctors/DoctorsList.jsx
@@ -14,9 +14,15 @@ const DoctorsList = () => {
       {error && <Error />}
       {!loading && !error && (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
-          {doctors.map((doctor) => (
-            <DoctorCard key={doctor.id} doctor={doctor} />
-          ))}
+          {doctors?.length > 0 ? (
+            doctors.map((doctor) => (
+              <DoctorCard key={doctor.id} doctor={doctor} />
+            ))
+          ) : (
+            <p className="text-center text-[18px] leading-6">
+              No doctors found at the moment.
+            </p>
+          )}
         </div>
       )}
     </>
